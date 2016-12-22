50917a2c9b10a42f9207de83c65e1d79dd440f82
Anton D. Kachalov <mouse@yandex-team.ru>
2016-12-22 15:38:20 +0300

    Add support for RMC/CB customization
    Signed-off-by: Anton D. Kachalov <mouse@yandex-team.ru>

--- meta-yandex/meta-openrack/meta-shaosi/recipes-phosphor/workbook/shaosi-config/Shaosi.py
+++ meta-yandex/meta-openrack/meta-shaosi/recipes-phosphor/workbook/shaosi-config/Shaosi.py
@@ -2,6 +2,13 @@
 ##   state can change to next state in 2 ways:
 ##   - a process emits a GotoSystemState signal with state name to goto
 ##   - objects specified in EXIT_STATE_DEPEND have started
+
+board = None
+try:
+    with open('/etc/openrack-board', 'r') as f:
+        board = f.read().split('-', 2)[0]
+except: pass
+
 SYSTEM_STATES = [
 	'BASE_APPS',
 	'BMC_STARTING',
@@ -20,8 +27,22 @@ EXIT_STATE_DEPEND = {
 FRU_INSTANCES = {
 	'<inventory_root>/system' : { 'fru_type' : 'SYSTEM','is_fru' : True, 'present' : "True" },
 	'<inventory_root>/system/systemevent' : { 'fru_type' : 'SYSTEM_EVENT', 'is_fru' : False, },
-	'<inventory_root>/system/board' : { 'fru_type' : 'BMC','is_fru' : False, 'manufacturer' : 'ASPEED' },
+	'<inventory_root>/system/board' : { 'fru_type' : board,'is_fru' : False, 'manufacturer' : 'ASPEED', 'eeprom' : '7-0056@16384' },
 }
+
+if board == 'RMC':
+    for i in range(1, 7):
+        n = i
+        if n > 3: n += 1
+        FRU_INSTANCES.update({
+            '<inventory_root>/system/CB{}'.format(i) : { 'fru_type' : 'CB','is_fru' : False, 'manufacturer' : 'ASPEED', 'eeprom' : '0-003{}@0'.format(n) },
+        })
+    for i in ('A', 'B'):
+        if i == 'A': n = '9'
+        else: n = 'b'
+        FRU_INSTANCES.update({
+            '<inventory_root>/system/RMC{}'.format(i) : { 'fru_type' : 'RMC','is_fru' : False, 'manufacturer' : 'ASPEED', 'eeprom' : '0-003{}@0'.format(n) },
+        })
 
 ID_LOOKUP = {
 	'FRU' : {
@@ -49,12 +70,42 @@ GPIO_CONFIG['POWER_BUTTON'] = { 'gpio_pin': 'H1', 'direction': 'both' }
 #GPIO_CONFIG['SLOT2_PRESENT'] =         { 'gpio_pin': 'N5', 'direction': 'in' }
 
 HWMON_CONFIG = {
-	'8-0048' :  {
+	'7-0048' :  {
 		'names' : {
 			'temp1_input' : { 'object_path' : 'temperature/ambient','poll_interval' : 5000,'scale' : -3,'units' : 'C' },
 		}
 	},
 }
+
+if board == 'CB':
+    HWMON_CONFIG.update({
+	'7-002f' : {
+		'names' : {
+			'pwm1' : { 'object_path' : 'speed/fan1','poll_interval' : 10000,'scale' : 1,'units' : '' },
+			'pwm2' : { 'object_path' : 'speed/fan2','poll_interval' : 10000,'scale' : 1,'units' : '' },
+			'pwm3' : { 'object_path' : 'speed/fan3','poll_interval' : 10000,'scale' : 1,'units' : '' },
+			'pwm4' : { 'object_path' : 'speed/fan4','poll_interval' : 10000,'scale' : 1,'units' : '' },
+			'pwm5' : { 'object_path' : 'speed/fan5','poll_interval' : 10000,'scale' : 1,'units' : '' },
+			'pwm6' : { 'object_path' : 'speed/fan6','poll_interval' : 10000,'scale' : 1,'units' : '' },
+			'fan1_input' : { 'object_path' : 'tach/fan1','poll_interval' : 10000,'scale' : 1,'units' : '' },
+			'fan2_input' : { 'object_path' : 'tach/fan2','poll_interval' : 10000,'scale' : 1,'units' : '' },
+			'fan3_input' : { 'object_path' : 'tach/fan3','poll_interval' : 10000,'scale' : 1,'units' : '' },
+			'fan4_input' : { 'object_path' : 'tach/fan4','poll_interval' : 10000,'scale' : 1,'units' : '' },
+			'fan5_input' : { 'object_path' : 'tach/fan5','poll_interval' : 10000,'scale' : 1,'units' : '' },
+			'fan6_input' : { 'object_path' : 'tach/fan6','poll_interval' : 10000,'scale' : 1,'units' : '' },
+		}
+	},
+	'7-0040' : {
+		'names' : {
+			'in1_input' : { 'object_path' : 'power/current','poll_interval' : 10000,'scale' : 10000,'units' : 'A' },
+		}
+	},
+	'7-0049' :  {
+		'names' : {
+			'temp1_input' : { 'object_path' : 'temperature/board','poll_interval' : 5000,'scale' : -3,'units' : 'C' },
+		}
+	},
+    })
 
 # Miscellaneous non-poll sensor with system specific properties.
 # The sensor id is the same as those defined in ID_LOOKUP['SENSOR'].
