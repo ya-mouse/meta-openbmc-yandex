c6a15c7be1f18d0ce7f27c5b70290f949e822673
Anton D. Kachalov <mouse@yandex-team.ru>
2017-01-24 17:52:05 +0300

    Add POST support
    Signed-off-by: Anton D. Kachalov <mouse@yandex-team.ru>

--- meta-yandex/meta-openrack/meta-shaosi/recipes-luarocks/lua-ipmi/files/api.lua
+++ meta-yandex/meta-openrack/meta-shaosi/recipes-luarocks/lua-ipmi/files/api.lua
@@ -4,4 +4,8 @@ function _M.get(self)
     return 'get:'..tostring(self.match.path)
 end
 
+function _M.post(self)
+    return 'POST:'..tostring(self.match.path)..':'..tostring(self.match.prop)
+end
+
 return _M
