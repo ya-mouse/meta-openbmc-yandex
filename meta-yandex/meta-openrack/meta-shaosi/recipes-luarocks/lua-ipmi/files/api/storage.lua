bd635333e4083671fe6213ec5d49b368b6a1e5de
Anton D. Kachalov <mouse@yandex-team.ru>
2017-02-08 16:39:32 +0300

    Fixup GET/POST
    Signed-off-by: Anton D. Kachalov <mouse@yandex-team.ru>

--- meta-yandex/meta-openrack/meta-shaosi/recipes-luarocks/lua-ipmi/files/api/storage.lua
+++ meta-yandex/meta-openrack/meta-shaosi/recipes-luarocks/lua-ipmi/files/api/storage.lua
@@ -13,9 +13,9 @@ local _M = { AUTHORIZE = true }
 function get_json()
     read_body()
     local body, err = get_body_data()
-    if not body then return nil, err end
+    if not body then return nil, 'body: '..tostring(err) end
     local js, err = cjson_decode(body)
-    if not js then return nil, err end
+    if not js then return nil, 'decode: '..tostring(err) end
     local data = js.data
     if not data then return nil, "No data" end
 
@@ -28,7 +28,7 @@ function _M.get(self)
         local key, data
         data = {}
         for key in gmatch(inkey, '[^,]+') do
-            log(ERR, key)
+--            log(ERR, key)
             data[key] = store_db:get(key)
         end
         return cjson_encode({ data = data, status = 'ok', message = '200 OK' })
@@ -39,10 +39,11 @@ end
 
 function _M.post(self)
     local js, err = get_json()
-    if not js then return err end
+    if not js then return cjson_encode({ status = 'fail', message = '400 BAD', error = err }) end
+
     if self.match.key ~= nil then
-        store_db:set(self.match.key, js)
-        return cjson_encode({ status = 'ok', message = '200 OK' })
+        local msg = store_db:set(self.match.key, js)
+        return cjson_encode({ status = 'ok', message = '200 OK', msg = tostring(msg) })
     end
 
     local k, v
