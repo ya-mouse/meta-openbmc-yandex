27e9c1615e1a6a8cfbe6a2beeb01d776a33137ac
kitsok <kitsok@yandex-team.ru>
2017-08-02 15:40:21 +0300

    Fixed bug with dropping SN in EEPROM

--- meta-yandex/meta-openrack/meta-shaosi/recipes-phosphor/setmacs/files/setmacs.sh
+++ meta-yandex/meta-openrack/meta-shaosi/recipes-phosphor/setmacs/files/setmacs.sh
@@ -37,7 +37,7 @@ read -p "Press ENTER to write data or CTRL-C to terminate script " rc
 
 echo "Please wait, don't terminate the script"
 # write data to eeprom
-printf "mac0=$mac0\nmac1=$mac1\nserial=$serial\n" | dd of=/sys/bus/i2c/devices/7-0056/eeprom bs=16k seek=1
+printf "mac0=$mac0\nmac1=$mac1\nserial=$serial_eep\n" | dd of=/sys/bus/i2c/devices/7-0056/eeprom bs=16k seek=1
 fw_setenv ethaddr ${mac0}
 fw_setenv eth1addr ${mac1}
 
