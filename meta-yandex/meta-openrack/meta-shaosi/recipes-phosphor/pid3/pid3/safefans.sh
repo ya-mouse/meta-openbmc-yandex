1a099b9ddaa97f068de71f712052c4814b9ac25e
kitsok <kitsok@yandex-team.ru>
2017-09-06 18:21:28 +0300

    Changed safefans.sh to accept argument and un-hardcoded location of hwmon with fans

--- meta-yandex/meta-openrack/meta-shaosi/recipes-phosphor/pid3/pid3/safefans.sh
+++ meta-yandex/meta-openrack/meta-shaosi/recipes-phosphor/pid3/pid3/safefans.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-for i in {1..8}; do 
-  echo 140 > /sys/class/hwmon/hwmon0/device/pwm${i}
+defaultfan=140
+if [ ! -z $1 ]; then
+  defaultfan=$1
+fi
+
+fgrep w83795g /sys/class/hwmon/hwmon*/uevent -l | while read hwmon; do
+    for pwm in ${hwmon%%/uevent}/device/pwm[1-8]; do
+        [ -f "$pwm" ] || continue
+        echo $defaultfan > $pwm
+    done
 done
 
